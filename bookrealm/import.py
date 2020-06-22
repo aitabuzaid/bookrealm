852de0e8b435e5fd9750e8a52e9b4a9b432e6416
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-06-22 22:15:20 +0300

    feat: added app factory init.py

--- bookrealm/import.py
+++ bookrealm/import.py
@@ -7,35 +7,38 @@ from queries import create_db, delete_db, copy_data, insert_row
 
 database_url = os.environ['DATABASE_URL']
 engine = create_engine(database_url)
+
 db = scoped_session(sessionmaker(bind=engine))
-conn = engine.raw_connection()
-cur = conn.cursor()
-file = open('books.csv')
+
 
 #db.execute(delete_db)
 def main():
     db.execute(delete_db)
     db.execute(create_db)
+    """
+    conn = engine.raw_connection()
+    cur = conn.cursor()
 
+    file = open('books.csv', "r")
+    #db.execute(copy_data)
+    print("hi3")
+    cur.copy_expert(copy_data, file)
+    print("hi4")
+    cur.commit()
+    file.close()
     """
     f = open("books.csv")
     reader = csv.reader(f, )
     print(next(reader,None))
     for isbn, title, author, year in reader:
-        #print(year)
         db.execute(insert_row,
                    {"isbn": isbn, "title": title,
                     "author": author, "year": int(year)})
                     
-    """
-    #db.execute(copy_data)
 
-    cur.copy_expert(copy_data, file)
-    cur.commit()
 
     db.commit()
 
-#
 
 
 if __name__ == "__main__":
