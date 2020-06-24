fbf06f7bdc23668bc00a6519f32573eb2ab7d3e3
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-06-24 09:02:39 +0300

    feat: added methods in auth.py

--- bookrealm/auth.py
+++ bookrealm/auth.py
@@ -22,10 +22,10 @@ def register():
         db = get_db()
         error = None
 
-        if not username:
-            error = 'Please select a username.'
-        elif not password:
-            error = 'Please select a password.'
+        if not (name and username and password and email):
+            error = 'Please fill all the fields.'
+        # elif not password:
+        #    error = 'Please select a password.'
         elif db.execute(
                 'SELECT id FROM users WHERE username = :username',
                 {"username": username}).fetchone() is not None:
@@ -35,10 +35,67 @@ def register():
             db.execute(
                 insert_user, {'name': name,
                               'username': username,
-                              'password': password,
+                              'password': generate_password_hash(password),
                               'email': email}
             )
             db.commit()
+            return redirect(url_for('auth/login'))
 
         flash(error)
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM users WHERE username = :username',
+            {"username": username}
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM users WHERE id = :id',
+            {"id": id}
+        ).fetchone()
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
+
