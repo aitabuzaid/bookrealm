654aaa16836f37f1689538c739f2b3d37af90ae9
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-06-30 22:56:33 +0300

    feat: added a new book view for reviews

--- bookrealm/books.py
+++ bookrealm/books.py
@@ -18,7 +18,7 @@ def index():
         db = get_db()
         books = db.execute(
         """
-        SELECT isbn, title, author, year 
+        SELECT *
         FROM books 
         WHERE LOWER(isbn) LIKE CONCAT('%',:query,'%')
         OR LOWER(title) LIKE CONCAT('%',:query,'%')
@@ -26,3 +26,19 @@ def index():
         """, {"query": query}
         ).fetchall()
     return render_template('books/index.html', books=books)
+
+
+@bp.route('/book/<int:id>', methods=('GET', 'POST'))
+def book(id):
+    db = get_db()
+    reviews = db.execute(
+        """
+        SELECT *
+        FROM books b
+        JOIN reviews r
+        ON b.id = r.book_id
+        WHERE b.id = :id
+        """,
+        {"id": id}
+    ).fetchall()
+    return render_template('books/book.html', reviews=reviews)
